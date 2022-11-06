a5f2ff50b12e110b40e415e8d9fbd108b5903066
LDudrey <113869466+LDudrey@users.noreply.github.com>
2022-11-06 00:12:39 -0500

    fixed timer to start countdown with start

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,58 +3,55 @@ var highScoreEl = document.querySelector(".highscore");
 var questionEl = document.querySelector(".question");
 var paraEl = document.querySelector(".para");
 var answerEl = document.querySelector(".answer");
+var startButton = document.querySelector(".start");
 
-var secondsLeft = 10;
 
 // Creates beginning page
 function renderCodeQuiz() {
-    questionEl.textContent = "Coding Quiz Challenge"
-    paraEl.textContent = "Try to answer the following code-related questions with in the time limit. Keep in mind that incorrect answers will penalize your score time by ten seconds!"
+    questionEl.textContent = "Coding Quiz Challenge";
+    paraEl.textContent = "Try to answer the following code-related questions with in the time limit. Keep in mind that incorrect answers will penalize your score time by ten seconds!";
+    startButton.addEventListener("click", startQuiz); 
 }
 
-// // Highscore
-// function viewHighScore() {
-//     addEventListener("click", function(event)) {
+function startQuiz() {
+    countDown();
 
-//     }
-// }
+
+}
+
+
+
+
+
 
 // Timer
-function setTime() {
+function countDown() {
+    var secondsLeft = 10;
     // Sets interval in variable
-    var timerInterval = setInterval(function () {
+    var timeInterval = setInterval(function () {
         if (secondsLeft > 0) {
             secondsLeft--;
             timeEl.textContent = "Time Left: " + secondsLeft;
         } else if (secondsLeft === 0) {
             timeEl.textContent = "Time Left: " + secondsLeft;
-            questionEl.textContent = "Times Up!"
-            clearInterval(timerInterval);
+            clearInterval(timeInterval);
             displayMessage();
         }
     }, 1000);
+};
+
+// Displays times up message and results of score?
+function displayMessage() {
+   questionEl.textContent = "Times Up!"
 }
 
-// // Displays times up message and results of score?
-// function displayMessage() {
-//     if (secondsLeft === 0)
-//    questionEl.textContent = "Times Up!"
+
+// Function to create and send message after all answers completed
+// function sendMessage() {
+//     timeEl.textContent = " ";
+
+
 // }
-
-
-// Function to create and append colorsplosion image
-function sendMessage() {
-    timeEl.textContent = " ";
-    var imgEl = document.createElement("img");
-    imgEl.setAttribute("src", "images/image_1.jpg");
-    mainEl.appendChild(imgEl);
-
-}
-
-setTime();
-
-
-
 
 
 
