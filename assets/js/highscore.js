acb32060e598c00f6a9fdd4b985a10b17f73cb4c
LDudrey <113869466+LDudrey@users.noreply.github.com>
2022-11-06 20:40:32 -0600

    got quiz questions to show on main page

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,8 +1,21 @@
 var scoreListEl = document.querySelector(".scorelist");
 var clearButton = document.querySelector(".clear");
+var scores = [];
 
 // recall local storage to an array over a list
+// The following fucntion renders items in a to list as <li> elements
+function renderScoreList() {
+    // Render a new li for each score
+    for (var i = 0; i < scores.length; i++) {
+      var score = scores[i];
+  
+      var li = document.createElement("li");
+      li.textContent = score;
+      li.setAttribute("data-index", i);
 
+      todoList.appendChild(li);
+    }
+  };
 
 //clear button clears local storage
 clearButton.addEventListener("click", clearList);
@@ -10,3 +23,15 @@ clearButton.addEventListener("click", clearList);
 function clearList() {
 localStorage.clear();
 }
+
+// This function is being called below and will run with the page loads
+function init() {
+    // Get stored scores from localStorage
+    var storedScores = JSON.parse(localStorage.getItem("scores"));
+    // If scores were retireived from localStorage, update the score array to it
+    if (storedScores !== null) {
+      todos = storedScores;
+    }
+    // This is a helper function that will render scores to the DOM
+    renderScoreList();
+  };
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,31 @@
 var timeEl = document.querySelector(".timer");
-var welcomeEl = document.querySelector(".welcome");
+var titleEl = document.querySelector(".title");
 var paraEl = document.querySelector(".para");
-var answerEl = document.querySelector(".answers");
-var startButton = document.querySelector(".start");
-var quizResult = document.querySelector("results");
-var quizContainer = document.querySelector("quiz");
-var submitButton = document.querySelector("submit");
+var quizContainer = document.getElementById("quiz");
+var questionsEl = document.getElementById("questions");
+var answerEl = document.querySelectorAll(".answers");
+var startButton = document.getElementById("start");
+var quizResult = document.querySelector(".results");
+var submitButton = document.querySelector(".submit");
+var hiddenEl = document.querySelector(".hidden");
 
 // Creates beginning page
 function renderCodeQuiz() {
-    welcomeEl.textContent = "Coding Quiz Challenge";
+    titleEl.textContent = "Coding Quiz Challenge";
     paraEl.textContent = "Try to answer the following code-related questions with in the time limit. Keep in mind that incorrect answers will penalize your score time by ten seconds!";
     startButton.addEventListener("click", showQuestions);
     startButton.addEventListener("click", countDown);
+    startButton.addEventListener("click", hide);
 }
+
+//https://stackoverflow.com/questions/52976589/hide-element-when-you-click-on-it-with-addeventlistener
+function hide() {
+    if (hiddenEl.style.display === "none") {
+        hiddenEl.style.display = "block";
+    } else {
+        hiddenEl.style.display = "none";
+    }
+};
 
 // Timer
 function countDown() {
@@ -26,82 +38,43 @@ function countDown() {
         } else if (secondsLeft === 0) {
             timeEl.textContent = "Time Left: " + secondsLeft;
             clearInterval(timeInterval);
-            displayMessage();
+            hide();
+            titleEl.textContent = "Times Up!"
+            paraEl.textContent = "Try to answer all the questions before the time runs out!"
+
         }
     }, 1000);
 };
 
+// https://www.youtube.com/watch?v=RswgVWKJRLM
+function showQuestions() {
+    questionsEl.textContent = quizQuestions.question;
+  answerEl.forEach(function(element, index) {
+    element.textContent = quizQuestions.answers[index];
+    element.addEventListener('click', function() {
+        if (quizQuestions.correctAnswer == index) {
+            console.log('Correct');
+        } else {
+            console.log('Wrong');
+        }
+    })
+  })
+};
+
 // Questions courtesy of https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS
-var quizQuestions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript?",
-        answers: {
-            a: "<javascript>",
-            b: "<js>",
-            c: "<script>",
-            d: "<scripting>"
-        },
-        correctAnswer: '3'
-    },
-    {
-        question: "How to write an IF statement in JavaScript?",
-        answers: {
-            a: "if (i == 5)",
-            b: "if i = 5 then",
-            c: "if i == 5 then",
-            d: "if i = 5"
-        },
-        correctAnswer: '1'
-    },
-];
+var quizQuestions = {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: ["<javascript>", "<js>", "<script>", "<scripting>"],
+    correctAnswer: 2
+};
 
-function showQuestions(quizQuestions) {
-    var output = [];
-	var answer;
-	// for each question...
-	for(var i=0; i< quizQuestions.length; i++){
-		
-		// first reset the list of answers
-		answer = [];
-		// for each available answer to this question...
-		for(letter in question[i].answer){
-
-			// ...add an html radio button
-			answer.push(
-				'<label>'
-					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
-					+ letter + ': '
-					+ question[i].answer[letter]
-				+ '</label>'
-			);
-		}
-		// add this question and its answers to the output
-		output.push(
-			'<div class="question">' + question[i].question + '</div>'
-			+ '<div class="answer">' + answer.join('') + '</div>'
-		);
-	}
-
-	// finally combine our output list into one string of html and put it on the page
-	quizContainer.innerHTML = output.join('');
-}
-
-// Displays times up message and results of score?
-function displayMessage() {
-    welcomeEl.textContent = "Times Up!"
-    paraEl.textContent = "Try to answer all the questions before the time runs out!"
-}
 
 
 // Function to show end score result after all answers completed
 // function quizResult () {
 //     questionEl.textContent = "All done!"
 //     paraEl.textContent = "Your score is" + scoreTotal;
-
-
 // }
-
-
 
 
 // The init() function fires when the page is loaded 
